93c6f33fb3889ff8c97260154f2376fa99a0bb82
sukunyan <gabriel.estevez@juanxxiii.net>
2025-03-14 17:03:54 +0100

    Add files via upload

--- Fabrica/src/main/java/Controlador/FabricaApplication.java
+++ Fabrica/src/main/java/Controlador/FabricaApplication.java
@@ -3,11 +3,18 @@ package Controlador;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import Vista.Consola;
+
 @SpringBootApplication
 public class FabricaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FabricaApplication.class, args);
+		
+		Consola consola = new Consola();
+		
+		consola.abrirNavegador("http://localhost:8080/");
+		
 	}
 
 }
