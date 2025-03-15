0e1f3f7dff2705ca3bd4d5b5b78f144f91e021f1
sukunyan <gabriel.estevez@juanxxiii.net>
2025-03-15 01:25:34 +0100

    Add files via upload

--- Fabrica/src/main/java/Controlador/FabricaApplication.java
+++ Fabrica/src/main/java/Controlador/FabricaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import Vista.Consola;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = "")
 public class FabricaApplication {
 
 	public static void main(String[] args) {
--- Fabrica/src/main/java/Vista/Consola.java
+++ Fabrica/src/main/java/Vista/Consola.java
@@ -57,6 +57,8 @@ public class Consola {
 	@PostMapping("/registro")
 	public ModelAndView registrarse(@RequestParam String Usuario, @RequestParam String Contraseña) {
 		
+		System.out.println("Recibida solicitud POST con usuario: " + Usuario);
+		
 		/*txt con el nombre del usuario*/
 		String ruta = "usuarios/" + Usuario + ".txt";
 		File Archivo = new File(ruta);
@@ -68,6 +70,7 @@ public class Consola {
             modelAndView.addObject("mensaje", "El nombre de usuario ya existe, por favor elige otro.");
             return modelAndView;
 		}
+
 		
 		//revision del archivo, guarda el usuario y la contraseña en el archivo
 		try(FileWriter fw = new FileWriter(Archivo, false); PrintWriter pw = new PrintWriter(fw)){
